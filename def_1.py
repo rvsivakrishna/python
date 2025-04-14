ee2afe074eb55fe2236e2e0fd82cdd23f3b63052
rvsivakrishna <rvsivakrishnaa@gmail.com>
2025-04-14 12:35:35 +0530

    Add files via upload

--- def_1.py
+++ def_1.py
@@ -1,6 +1,16 @@
-def total_bill(price):
-    total = 12%price
+price = float(input("Price of the Goods:" ))
+qty = int(input("no of goods: "))
+tax = 0.02
+def get_total(price, qty, tax ,dd):
+    subtotal = price * qty * (1-dd)
+    print(f"Total amount to be paid: {subtotal *(1+tax)}.")  
+###########################################################################
 
-    print (total)
+dd=float(input("DD: "))
+if dd != 0.01:
+    dd = float(input("new DD: "))
+get_total(price, qty,tax,dd)
 
-total_bill(100000)
+
+    
+
